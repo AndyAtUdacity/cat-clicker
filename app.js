a5e59647dc0de7a766e531c6f24f1616f7a45f61
Andy Brown <pursuingpareto@gmail.com>
2014-10-28 18:23:04 -0700

    refactor as MVOctopus. Clean code FTW.

--- app.js
+++ app.js
@@ -1,33 +1,78 @@
-
-var cats = $(".cat");
-var buttons = $("button");
-
-function hideAllCats(){
-	for (var i=0; i<cats.length; i++){
-		$(cats[i]).hide();
+var model = {
+	numCats : 5,
+	cats : [],
+	currentCat: null,
+	Cat  : function(){
+		this.count = 0;
+		this.image = 'cat_picture1.jpeg';
+		this.incrementCount = function(){
+			this.count += 1;
+		}
+	},
+	setup : function(){
+		for (var i=0; i<this.numCats; i++){
+			var newCat = new this.Cat;
+			newCat.image = 'cat_picture'+i+'.jpeg';
+			this.cats.push(newCat);
+		}
+		this.currentCat = this.cats[0];
 	}
 }
 
-function bindButtonToCat(idNumber){
-	$("#button"+idNumber).click(function(){
-		hideAllCats();
-		$("#cat"+idNumber).show();
-	})
+var octopus = {
+	incrementCat : function() {
+		model.currentCat.count++;
+		catView.render();
+		catListView.render();
+	},
+	getCurrentCat : function() {
+		return model.currentCat;
+	},
+	getNumCats : function() {
+		return model.cats.length;
+	},
+	chooseCat : function(catNumber){
+		model.currentCat = model.cats[catNumber];
+		catListView.render();
+		catView.render();
+	}
+};
+
+var catListView = {
+	buttons : $('#cat-list'),
+	setup : function(){
+		for (var i=0; i<octopus.getNumCats(); i++){
+			this.buttons.append(this.makeButtonHTML(i));
+		}
+	},
+	render : function(){
+	},
+	makeButtonHTML : function(catNumber){
+		return $('<button/>',
+		{
+			text: 'Cat '+catNumber,
+			click: function() {
+				octopus.chooseCat(catNumber);
+			}
+		})
+	}
 }
 
-function bindCounterToCat(idNumber){
-	var cat = "#cat"+idNumber
-	$(cat).click(function(){
-		var count = $(cat+" > .counter").text();
-		count = parseInt(count) + 1;
-		$(cat+" > .counter").text(count);
-	})
+var catView = {
+	catDisplay : $('#cat-display'),
+	render : function(){
+		var cat = octopus.getCurrentCat();
+		$('#cat-image').attr("src", cat.image);
+		$('#count').html(cat.count);
+	},
+	setup : function(){
+		this.render();
+		$('#cat-image').click(function() {
+			octopus.incrementCat();
+		})
+	}
 }
 
-for (var i=1; i<=buttons.length; i++){
-	bindButtonToCat(i);
-}
-
-for (var i=1; i<=cats.length; i++){
-	bindCounterToCat(i);
-}
+model.setup();
+catListView.setup();
+catView.setup();
